2974f00bd8f260454f9167e2d384714e2cadf897
Hana <bahloul.hana@gmail.com>
2023-09-16 22:03:13 +0100

    create a module called retrofit and store the interface classes required for retrofit.

---
+++ app/src/main/java/com/example/countriesapplication/retrofit/CountriesApi.kt
@@ -0,0 +1,10 @@
+package com.example.countriesapplication.retrofit
+
+import com.example.countriesapplication.model.Country
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface CountriesApi {
+    @GET("DevTides/countries/master/countriesV2.json") // this is the end point that we need for this project to get the api response.
+    suspend fun getCountries(): Response<List<Country>> // we are using coroutines here so making the function suspend.
+}
